b7f89ab2ff120fbca40105d65b2698d404d7074c
Alan <alan.vandijk@gmail.com>
2017-11-12 22:18:47 -0500

    ngrams for lyrics

--- lexicalFeatureGenerator.py
+++ lexicalFeatureGenerator.py
@@ -1,19 +1,33 @@
 from lyricsFetch import lyricsFetch
 import re
 
-num = 10
-lf = lyricsFetch('rock', num, 5)
+def get_lyric_ngrams(artistCount,songCount,category='rock',ngram=3):
+	lf = lyricsFetch('rock', artistCount, songCount)
 
-lyrics = []
-for i in range(num-1):
-	lyrics.append(lf.getNextLyricSet()[2])
+	lyrics = []
+	for i in range(artistCount-1):
+		lyrics.append(lf.getNextLyricSet()[2])
 
-allLines = []
-for l in lyrics:
-	lines_str = re.sub('\n\n', '\n', l)
-	lines = lines_str.split('\n')
-	lines = [re.sub(r'([,!?])', r' \1', x) for x in lines]
-	for li in lines:
-		allLines.append(li)
+	allLines = []
+	for l in lyrics:
+		lines_str = re.sub('\n\n', '\n', l)
+		lines = lines_str.split('\n')
+		lines = [re.sub(r'([,!?]|\.{3})', r' \1', x) for x in lines]
+		for li in lines:
+			allLines.append(li)
+	return get_ngrams(allLines, ngram)
 
-print allLines[:10]
+# given a list of lines, returns a flattened list of ngrams, including start and stop tokens
+# if n > len(line) + 2, it will return [<START>] + line + [<STOP>]
+def get_ngrams(lines, n):
+	ngrams = []
+	for line in lines:
+		line_formatted = ['<S_L>'] + line.split() + ['<E_L>']
+		if len(line) < n-1:
+			ngrams.append(line_formatted)
+		else:
+			# expression for the number of ngrams in a sequence
+			num_ngram = len(line_formatted) - n + 1
+			for i in range(num_ngram):
+				ngrams.append(line_formatted[i:i+n])
+	return ngrams
