55677beee049c6b1ebe16f8a04a640cbcd602488
Alan <alan.vandijk@gmail.com>
2017-12-19 13:28:41 -0500

    prob-weighted ngram selection

--- lexicalFeatureGenerator.py
+++ lexicalFeatureGenerator.py
@@ -2,11 +2,14 @@
 import re
 from nltk.corpus import cmudict
 import time
+import numpy as np
+import os
 
 cmud = cmudict.dict()
 cmuw = cmudict.words()
 vowels = ['AA', 'AH', 'AW', 'EH', 'EY', 'IH', 'OW', 'UH', 'AE', 'AO', 'AY', 'ER', 'IY', 'OY', 'UW']
 fixed = {}
+rhyme2words = {}
 
 def get_lyric_ngrams(artistCount,songCount,category='rock',ngram=3):
 	lf = lyricsFetch('rock', artistCount, songCount)
@@ -30,6 +33,23 @@ def get_ngrams(lines, n):
 	ngrams = []
 	for line in lines:
 		line_formatted = ['<S_L>'] + line.split() + ['<E_L>']
+		if len(line) < n-1:
+			ngrams.append(line_formatted)
+		else:
+			# expression for the number of ngrams in a sequence
+			num_ngram = len(line_formatted) - n + 1
+			for i in range(num_ngram):
+				ngrams.append(line_formatted[i:i+n])
+	return ngrams
+
+def get_ngrams_normalized(text, n):
+	text = re.sub('[\[\]!?().,":;]', '', text) # strip bad characters
+	songs = text.split('\n\n<SONG_BOUNDARY>\n\n')
+	lines = [line for song in songs for stanza in song.split('\n\n') for line in stanza.split('\n')]
+	ngrams = []
+	for line in lines:
+		line_formatted = [w.lower() for w in line.split()]
+		line_formatted.append('<endline>')
 		if len(line) < n-1:
 			ngrams.append(line_formatted)
 		else:
@@ -66,7 +86,7 @@ def get_ngram_featvec(text, n):
 	encoder = {v:i for i,v in enumerate(unique)}
 	decoder = {i:v for i,v in enumerate(unique)}
 	featvec = [encoder[ngram] for ngram in ngrams]
-	# return (featvec, encoder, decoder)
+	return featvec, encoder, decoder
 
 def getRhymePhonemes(word):
 	nonum = lambda x: re.sub('\d', '', x)
@@ -161,24 +181,41 @@ def processFixed():
 		else:
 			continue
 
-# processFixed()
 
-f = open("rock.txt", 'r')
-# lines = [song for song in f.read().split('\n')]
-songs = [song for song in f.read().split('\n\n<SONG_BOUNDARY>\n\n')]
+def generatePatterns(genre):
+	f = open("%s.txt" % (genre), 'r')
+	processFixed()
 
-processFixed()
+	patterns = open("patterns.txt", 'w')
 
-patterns = open("patterns.txt", 'w')
+	songs = f.read().split('\n\n<SONG_BOUNDARY>\n\n')
 
-start = time.time()
-for i,s in enumerate(songs):
-	s = re.sub(' \n', '\n', s)
-	rhyme = rhyme_pattern(s)
-	rhyme = re.sub('<RH=', '', rhyme)
-	rhyme = re.sub('>', '', rhyme)
-	rstanza = rhyme.split(',')
-	for rhymePattern in rstanza:
-		patterns.write(rhymePattern)
+	# start = time.time()
+	for i,s in enumerate(songs):
+		if i%50==0: print i/len(songs),'%'
+		s = re.sub(' \n', '\n', s)
+		rhyme = rhyme_pattern(s)
+		rhyme = re.sub('<RH=', '', rhyme)
+		rhyme = re.sub('>', '', rhyme)
+		rstanza = rhyme.split(',')
+		for rhymePattern in rstanza:
+			if 'X' in rhymePattern: continue
+			if max([int(r) for r in rhymePattern.split(' ')]) < 6:
+				patterns.write(rhymePattern + '\n')
+
+
+generatePatterns('rock')
+# N = 4
+
+# ngrams = [tuple(x) for x in get_ngrams_normalized(f.read(),N)]
+# lasts = [ngram for ngram in ngrams if list(ngram)[-1] == '<endline>']
+# from gauss import backGenLine
+
+# for _ in range(20):
+# 	arr = backGenLine(ngrams, np.random.choice(lasts,size=1)[0][N-2], 10)
+# 	string = ""
+# 	for a in arr:
+# 		string = string + a + ' '
+# 	print string[:-1]
 
 # print "TOOK: " + str(time.time() - start) + ' seconds'
